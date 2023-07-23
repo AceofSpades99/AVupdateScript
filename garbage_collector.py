40baa810eac12e5ff85a30f5b0f2e78a7a0c2ac5
Ace <dalejan051@gmail.com>
2023-07-23 03:25:27 -0400

    Clean unused files after download

---
+++ garbage_collector.py
@@ -0,0 +1,25 @@
+import os
+
+
+def clean_unused(update_ver, env):
+	files = []
+	for modules in update_ver:
+		if 'file' in update_ver[modules].keys():
+			files.append(update_ver[modules]['file'])
+	files.append('update.ver')
+	scandir(files, env)
+
+
+def scandir(files, env):
+	unused = []
+	for items in os.scandir(env['save_path']):
+		if items.name not in files:
+			unused.append(items.name)
+	print('Buscando archivos innecesarios luego de la actualización: ')
+	if unused:
+		print(f'Se eliminarán {len(unused)} archivos sin usar')
+		for files in unused:
+			os.remove(os.path.join(env['save_path'], files))
+		print('Archivos eliminados')
+	else:
+		print('No se han encontrado archivos sin usar')

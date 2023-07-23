3add638830cd39f8a969172ea0aa9165746b472f
Ace <dalejan051@gmail.com>
2023-07-23 03:27:56 -0400

    Defined a version number, made a starting point for the app, added the garbage_collector

--- main.py
+++ main.py
@@ -4,52 +4,45 @@ import urllib3
 import dotenv
 from pathlib import Path
 from time import sleep
+
+from env_manager import initialize
 from downloader import download
+from garbage_collector import clean_unused
 from parser import url_parser, file_parser
 from update_checker import get_updates
 
-env_path = os.path.expanduser('~/Documents/auto_up.env')
-save_path = ''
-url = 'https://antivirus.uclv.edu.cu/eset_upd/eset_nod32_antivirus_eav'
-if os.path.exists(env_path):
-	save_path = dotenv.get_key(env_path, 'save_path')
-	url = dotenv.get_key(env_path, 'url')
-else:
-	with open(env_path, 'w+') as o:
-		save_path = input('Ubicacion hacia donde descargar las actualizaciones: ')
-		dotenv.set_key(env_path, 'save_path', save_path.replace('\\', '/'))
-	new_url = input(
-		'Cambiar la direccion de los archivos de actualizacion? (y/n) (por defecto: '
-		'https://antivirus.uclv.edu.cu/nod32/update_all): ')
-	if new_url.strip().lower() == 'y':
-		url = input('Entre la nueva direccion: ')
-		dotenv.set_key(env_path, 'url', url)
-	else:
-		dotenv.set_key(env_path, 'url', url)
 
-try:
-	print('Iniciando (este proceso puede demorar unos segundos)')
-	first = url_parser(url)
-	if Path.is_file(Path(os.path.join(save_path, 'update.ver'))):
-		second = file_parser(os.path.join(save_path, 'update.ver'))
-		updates = get_updates(first, second)
-	else:
-		updates = get_updates(first)
-	if updates:
-		print('Descargando: ')
-		asyncio.run(download(os.path.join(save_path, 'update.ver'), url + '/update.ver', updates))
-	else:
-		print('No hay actualizaciones pendientes')
-except KeyboardInterrupt:
-	print('User interrupted the execution')
-except RuntimeError:
-	print('User interrupted the execution')
-except urllib3.exceptions.MaxRetryError:
-	print('Connection timed out, check your internet connection')
-except KeyError:
-	key = dotenv.get_key(env_path, 'url')
-	print(f'Could not reach: {key}\nMay be using a VPN')
-except:
-	print('Unknown error')
+def __version__():
+	return '1.6'
 
-sleep(5)
+
+if __name__ == '__main__':
+	env = initialize(__version__())  # check env to know if the app has been used before
+	try:
+		print('Iniciando (este proceso puede demorar unos segundos)')
+		online = url_parser(env['url'])  # parse the default_url's update.ver file
+		if Path.is_file(Path(os.path.join(env['save_path'], 'update.ver'))):  # if the download path has an update file:
+			local = file_parser(os.path.join(env['save_path'], 'update.ver'))
+			updates = get_updates(online, local)
+		else:
+			updates = get_updates(online)
+		if updates:  # if there are updates
+			print('Descargando: ')
+			asyncio.run(download(os.path.join(env['save_path'], 'update.ver'), env['url'] + '/update.ver', updates))
+			clean_unused(online, env)
+		else:
+			print('No hay actualizaciones pendientes')
+	except KeyboardInterrupt:
+		print('User interrupted the execution')
+	except RuntimeError:
+		print('User interrupted the execution')
+	except urllib3.exceptions.MaxRetryError:
+		print('Connection timed out, check your internet connection')
+	except KeyError:
+		key = dotenv.get_key(env['env_path'], 'url')
+		print(f'Could not reach: {key}\nMay be using a VPN')
+	except:
+		print('Unknown error')
+	
+	print('Ejecución finalizada')
+	sleep(5)

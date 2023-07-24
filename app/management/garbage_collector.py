151f9a5b044b0fc3d88dbb4cdcf41dbf15a90096
aceofspades99 <dalejan051@gmail.com>
2023-07-23 21:49:14 -0400

    Added file extension verification before file deletion just in case

--- app/management/garbage_collector.py
+++ app/management/garbage_collector.py
@@ -1,19 +1,23 @@
 import os
 
+file_extensions = (
+	'.nup',
+	'.ver',
+)
+
 
 def clean_unused(update_ver, env):
 	files = []
 	for modules in update_ver:
 		if 'file' in update_ver[modules].keys():
 			files.append(update_ver[modules]['file'])
-	files.append('update.ver')
 	scandir(files, env)
 
 
 def scandir(files, env):
 	unused = []
 	for items in os.scandir(env['save_path']):
-		if items.name not in files:
+		if items.name not in files and items.name.endswith(file_extensions):
 			unused.append(items.name)
 	print('Buscando archivos innecesarios luego de la actualización: ')
 	if unused:

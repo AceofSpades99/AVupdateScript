bf4f7be13a17c624a6294bcbac97b9ca3a0cd710
aceofspades99 <dalejan051@gmail.com>
2023-07-27 21:16:45 -0400

    Added a missing module's scan

--- app/update_downloader/update_checker.py
+++ app/update_downloader/update_checker.py
@@ -1,41 +1,35 @@
-def get_updates(online, local=None):
-    upd = {}
-    # if there are files already on the destination folder
-    if local is not None:
-        # for each element that exists on the online versions file
-        for item in online:
-            # if that element has a version and a file associated to it
-            if 'version' in online[item] and 'file' in online[item]:
-                # if the element does not have a local version or has an outdated version, add it to the queue
-                if item not in local or online[item]['version'] > local[item]['version']:
-                    upd[item] = updater(upd, item, online)
-    # if there are no files just add all
-    else:
-        for item in online:
-            if 'version' in online[item] and 'file' in online[item]:
-                upd[item] = updater(upd, item, online)
-    if upd:
-        upd['[VER]'] = 'update.ver'
-    return upd
+import os
+
+
+def get_updates(online, env, local=None):
+	upd = {}
+	# if there are files already on the destination folder
+	if local is not None:
+		# for each element that exists on the online versions file
+		for item in online:
+			# if that element has a version and a file associated to it
+			if 'versionid' in online[item] and 'file' in online[item]:
+				# if the element does not have a local version, has an outdated version
+				# or if the module file does not exist, add it to the queue
+				if item not in local or online[item]['versionid'] > local[item]['versionid'] \
+						or online[item]['file'] not in scan_local_files(env):
+					upd[item] = updater(upd, item, online)
+	# if there are no files just add all
+	else:
+		for item in online:
+			if 'version' in online[item] and 'file' in online[item]:
+				upd[item] = updater(upd, item, online)
+	if upd:
+		upd['[VER]'] = 'update.ver'
+	return upd
 
 
 def updater(upd, item, online):
-    upd[item] = {}
-    upd[item] = online[item]['version']
-    upd[item] = online[item]['file']
-    return upd[item]
+	upd[item] = {}
+	upd[item] = online[item]['version']
+	upd[item] = online[item]['file']
+	return upd[item]
 
-# def get_updates(online, local):   #if comparing 2 local files
-#     upd = {}
-#     for item in online:
-#         if online[item]['version'] > local[item]['version']:
-#             upd['Newer'] = 'online'
-#             upd[item] = {}
-#             upd[item] = online[item]['version']
-#             upd[item] = online[item]['file']
-#         elif online[item]['version'] < local[item]['version']:
-#             upd['Newer'] = 'local'
-#             upd[item] = {}
-#             upd[item] = local[item]['version']
-#             upd[item] = local[item]['file']
-#     return upd
+
+def scan_local_files(env):
+	return [file.name for file in os.scandir(env['save_path'])]

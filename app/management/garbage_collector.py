6316959281ff02fdba13802e4ae9ec0d49993c43
aceofspades99 <dalejan051@gmail.com>
2023-08-01 14:47:54 -0400

    Updated garbage_collector to compare online and offline update.ver files and scan the save directory looking for unneeded files

--- app/management/garbage_collector.py
+++ app/management/garbage_collector.py
@@ -6,25 +6,31 @@ file_extensions = (
 )
 
 
-def clean_unused(update_ver, env):
-	files = []
-	for modules in update_ver:
-		if 'file' in update_ver[modules].keys():
-			files.append(update_ver[modules]['file'])
-	files.append('update.ver')
-	scandir(files, env)
-
-
-def scandir(used, env):
-	unused = []
-	for items in os.scandir(env['save_path']):
-		if items.name not in used and items.name.endswith(file_extensions):
-			unused.append(items.name)
+def start_cleaning_process(online, offline, env):
 	print('Buscando archivos innecesarios luego de la actualización: ')
-	if unused:
-		print(f'Se eliminarán {len(unused)} archivos sin usar')
-		for files in unused:
-			os.remove(os.path.join(env['save_path'], files))
-		print('Archivos eliminados')
+	online_files, offline_files = scandir(online), scandir(offline)
+	difference = list(set(offline_files) - set(online_files))
+	if difference:
+		print(f'Se eliminarán {len(difference)} archivos sin usar')
+		print(difference)
+		clean_unused(difference, env)
 	else:
 		print('No se han encontrado archivos sin usar')
+
+
+def clean_unused(unused, env):
+	for files in unused:
+		os.remove(os.path.join(env['save_path'], files))
+	print('Archivos eliminados')
+
+
+def scandir(update_ver):
+	if update_ver:
+		files = []
+		for modules in update_ver:
+			if 'file' in update_ver[modules].keys():
+				files.append(update_ver[modules]['file'])
+		files.append('update.ver')
+		return files
+	else:
+		return []
